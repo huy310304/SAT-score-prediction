dbb3e5ec182118f7f5c884d745983b6d2b4c9b7d
Huy Nguyen <114793725+huy310304@users.noreply.github.com>
2023-08-06 03:11:46 +0700

    Add files via upload

---
+++ classification/sat_classification_pca.py
@@ -0,0 +1,218 @@
+# SAT score prediction project using classification techniques that split SAT score into 12 brackets with range 100
+# Implement PCA techniques and data generation by using the GPA_Big.csv file
+
+import time
+import torch
+from torch import nn, optim
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import r2_score, mean_absolute_error
+
+# data path
+data_location = 'GPA_Big.csv'
+
+# read the data csv file
+data = pd.read_csv(data_location)
+data = data.sample(frac=1)
+
+# Categorize into 12 brackets of SAT score with range 100 each
+data.loc[:, "SAT"] = (data["SAT"] - 400) // 100
+
+# convert hsize and hsrank into binary
+def binary_convert(feature):
+
+    hsize_binary_list = list()
+    for each in data[feature]:
+        b_list = []
+        binary = bin(each)[2:]
+        # convert into 10 digits binary by adding 0
+        while len(binary) < 10:
+            binary = "0" + binary 
+        
+        for b in binary:
+            b_list.append(int(b))
+
+        hsize_binary_list.append(b_list)
+
+    binary_array = np.array(hsize_binary_list)
+    return binary_array
+
+hsize_binary_array = binary_convert("hsize")
+hsize_loc = 4
+for i in range(10):
+    data.insert(hsize_loc+i, "hsize_binary", hsize_binary_array.T[i], allow_duplicates=True)
+
+hsrank_binary_array = binary_convert("hsrank")
+hsrank_loc = 15
+for i in range(10):
+    data.insert(hsrank_loc + i, "hsrank_binary", hsrank_binary_array.T[i], allow_duplicates=True)
+
+data = data.drop(['hsize', 'hsrank'], axis=1)
+
+# test_data is the last 400 entries
+test_data = data[-800:]
+data = data[:-800]
+
+target_feature = ['SAT']
+
+# drop the SAT feature, move to targets
+features = data.drop(target_feature, axis=1) 
+targets = data[target_feature]
+test_features = test_data.drop(target_feature, axis=1)
+test_targets = test_data[target_feature]
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+
+features = sc.fit_transform(features)
+features = pca.fit_transform(features)
+
+test_features = sc.transform(test_features)
+test_features = pca.transform(test_features)
+
+# validation data is the last 400 entries
+train_features, train_targets = features[:-800], targets[:-800]
+val_features, val_targets = features[-800:], targets[-800:]
+
+train_targets = train_targets.to_numpy()
+trainloader = np.concatenate((train_features, train_targets), axis=1)
+
+val_targets = val_targets.to_numpy()
+valloader = np.concatenate((val_features, val_targets), axis=1)
+
+test_targets = test_targets.to_numpy()
+testloader = np.concatenate((test_features, test_targets), axis=1)
+
+# convert train data into tensor
+trainloader = torch.tensor(trainloader, dtype=torch.float64)
+trainloader = torch.utils.data.DataLoader(trainloader, batch_size = 128, shuffle = True)
+
+# convert validation data into tensor
+valloader = torch.tensor(valloader, dtype=torch.float64)
+valloader = torch.utils.data.DataLoader(valloader, batch_size = 64, shuffle = True)
+
+# convert test data into tensor
+testloader = torch.tensor(testloader, dtype=torch.float64)
+
+
+model = nn.Sequential(nn.Linear(2, 256),
+                       nn.ReLU(),
+                       nn.Dropout(p = 0.2),
+                       nn.Linear(256, 256),
+                       nn.ReLU(),
+                       nn.Dropout(p = 0.2),
+                       nn.Linear(256, 128),
+                       nn.ReLU(),
+                       nn.Dropout(p = 0.2),
+                       nn.Linear(128, 128),
+                       nn.ReLU(),
+                       nn.Dropout(p = 0.2),
+                       nn.Linear(128, 64),
+                       nn.ReLU(),
+                       nn.Dropout(p = 0.2),
+                       nn.Linear(64, 16),
+                       nn.ReLU(),
+                       nn.Dropout(p = 0.2),
+                       nn.Linear(16, 12),
+                       nn.LogSoftmax(dim=1))
+
+# Change the dtype of weights and biases to torch.float64 to match the dtype of inputs
+new_dtype = torch.float64  
+
+for param in model.parameters():
+    param.data = param.data.to(new_dtype)
+
+loss_fn = nn.NLLLoss()
+optimizer = optim.Adam(model.parameters(), lr = 0.002)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=10)
+
+epochs = 200
+train_losses = []
+val_losses = []
+accuracies = []
+times = []
+
+
+for e in range(epochs):
+
+    start_epoch = time.time()
+
+    training_loss = 0
+    for row in trainloader:
+        features = row[:, :-1]
+
+        targets = row[:, -1].unsqueeze(1)
+        targets = targets.view(targets.shape[0])
+        targets = targets.type(torch.LongTensor)
+        
+        log_ps = model(features)
+        loss = loss_fn(log_ps, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        training_loss += loss.item()
+    else: 
+        validation_loss = 0
+        accuracy = 0
+
+        scheduler.step(training_loss)
+        
+        with torch.no_grad():
+            
+            model.eval()
+
+            for row in valloader:
+                features = row[:, :-1]
+                
+                targets = row[:, -1].unsqueeze(1)
+                targets = targets.view(targets.shape[0])
+                targets = targets.type(torch.LongTensor)
+                
+                log_ps = model(features)
+                validation_loss += loss_fn(log_ps, targets)
+
+                ps = torch.exp(log_ps)
+                top_p, top_class = ps.topk(1, dim = 1)
+                equals = top_class == targets.view(top_class.shape)
+                accuracy += torch.mean(equals.type(torch.FloatTensor))
+
+            model.train()
+
+        train_losses.append(training_loss/len(trainloader))
+        val_losses.append(validation_loss/len(valloader))
+        accuracies.append(accuracy/len(valloader))
+
+    end_epoch = time.time()
+    epoch_time = end_epoch - start_epoch
+    times.append(epoch_time)
+
+    print(f"Epoch: {e+1}/{epochs}")
+    print(f"Training Loss: {training_loss/len(trainloader)}")
+    print(f"Validation Loss: {validation_loss/len(valloader)}")
+    print(f"Validation Accuracy: {accuracy/len(valloader)}")
+
+plt.plot(times, label = "Time")
+plt.plot(train_losses, label = "Training loss")
+plt.plot(val_losses, label = "Validation loss")
+plt.plot(accuracies, label="Accuracy")
+plt.legend()
+plt.show()
+
+SAT_predictions = torch.exp(model(testloader[:, :-1]))
+SAT_targets = testloader[:, -1]
+SAT_targets = SAT_targets.type(torch.LongTensor)
+top_p, top_class = SAT_predictions.topk(1, dim = 1)
+equals = top_class == SAT_targets.view(top_class.shape)
+test_accuracy = torch.mean(equals.type(torch.FloatTensor))
+print(f"Test_accuracy for 800 test entries is: {test_accuracy}")
+
+# Plot predicting results
+plt.plot(top_class.detach().numpy(), label='SAT_prediction')
+plt.plot(SAT_targets.detach().numpy(), label='SAT_targets')
+plt.legend()
+plt.show()
